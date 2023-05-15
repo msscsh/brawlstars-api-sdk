4173388c1716a78ca87bcd4920b1a00b4e3fb0e5
msscsh <itsbruno42@gmail.com>
2023-05-15 18:27:08 -0300

    change project name to be releated with SDK

--- src/sdk/brawlstars-api.ts
+++ src/sdk/brawlstars-api.ts
@@ -5,6 +5,7 @@ import { Player } from "../types/player";
 export class BrawlStarsAPI extends Base {
 
     getPlayer(tag: string): Promise<Player> {
+        console.log(tag);
         return this.invoke(`/players/${tag}`);
     }
 
