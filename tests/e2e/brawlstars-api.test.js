248123bfe715d372b6d993d1bb52b257e6a5d33e
msscsh <itsbruno42@gmail.com>
2023-05-15 12:50:37 -0300

    ajustes

---
+++ tests/e2e/brawlstars-api.test.js
@@ -0,0 +1,10 @@
+import {jest, expect, test, describe} from '@jest/globals'
+
+// const { getPlayer } = require('../../src/api/brawlstars-api');
+// const playerTag = '#2QPVJ099C';
+
+describe('See if all the services used is online', () => {
+  test.todo('Service 1 is up - Should return 200')
+})
+
+  
--- tests/index.test.js
+++
@@ -1,6 +0,0 @@
-const index = require('../src/index')
-test('Can create a brawler object', () => {
-    const brawler = index.getPlayer();
-    expect(brawler).toBeUndefined();
-})
-  
